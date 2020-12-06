121e451aff74348e6329b074a8b765d6dc609321
davisantosr <davi-rocha2@hotmail.com>
2020-12-05 23:00:16 -0300

    created function based view using api_view decorator|fixed serializers indent

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -16,18 +16,18 @@ class ArticleSerializer(serializers.Serializer):
   updated_at = serializers.DateTimeField(read_only=True)
 
 
-def create(self, validated_data):
-  print(validated_data)
-  return Article.objects.create(**validated_data)
+  def create(self, validated_data):
+    print(validated_data)
+    return Article.objects.create(**validated_data)
 
-def update(self, instance, validated_data):
-  instance.author = validated_data.get('author', instance.author)
-  instance.title = validated_data.get('title', instance.title)
-  instance.description = validated_data.get('description', instance.author)
-  instance.body = validated_data.get('body', instance.body)
-  instance.location = validated_data.get('location', instance.location)
-  instance.publication_date = validated_data.get('date', instance.publication_date)
-  instance.active = validated_data.get('active', instance.active)
+  def update(self, instance, validated_data):
+    instance.author = validated_data.get('author', instance.author)
+    instance.title = validated_data.get('title', instance.title)
+    instance.description = validated_data.get('description', instance.author)
+    instance.body = validated_data.get('body', instance.body)
+    instance.location = validated_data.get('location', instance.location)
+    instance.publication_date = validated_data.get('date', instance.publication_date)
+    instance.active = validated_data.get('active', instance.active)
 
-  instance.save()
-  return instance
+    instance.save()
+    return instance
