dc4cd318c53c33756029a91f3941194408570492
davisantosr <davi-rocha2@hotmail.com>
2020-12-05 23:43:45 -0300

    class based views|using APIView class

--- news/api/views.py
+++ news/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -49,6 +50,21 @@ def article_detail_api_view(request, pk):
   elif request.method == "DELETE":
     article.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ArticleListCreateApiView(APIView):
+
+  def get(self, request):
+    articles = Article.objects.filter(active=True)
+    serializer = ArticleSerializer(articles, many=True)
+    return Response(serializer.data)
+
+  def post(self, request):
+    serializer = ArticleSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
     
