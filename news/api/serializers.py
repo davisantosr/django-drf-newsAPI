a994b2a2451555f2a3583422192a36e02fce5727
davisantosr <davi-rocha2@hotmail.com>
2020-12-06 16:37:01 -0300

    Adding SerializerMethodField performing extra fields

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 
+from datetime import datetime
+from django.utils import timesince
+
 from news.models import Article
 
 
 
 class ArticleSerializer(serializers.ModelSerializer):
 
+  time_since_publication = serializers.SerializerMethodField()
+
   class Meta:
     model = Article
     fields = ("__all__")
+    #exclude =  ("id", )
+    #fields = ("id", title, description, )
     
-
-
+  def get_time_since_publication(self, object):
+    publication_date = object.publication_date
+    now = datetime.now()
+    time_delta = timesince(publication_date, now)
+    return time_delta
 
 # =====================================================
 
