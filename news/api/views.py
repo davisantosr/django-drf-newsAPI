57b0a459d2dbb837dffedc5b6550c090e1bac967
davisantosr <davi-rocha2@hotmail.com>
2020-12-05 23:53:32 -0300

    class based views APIView|adding post, put, delete methods

--- news/api/views.py
+++ news/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
 
 from news.models import Article
 from news.api.serializers import ArticleSerializer
@@ -52,7 +53,7 @@ def article_detail_api_view(request, pk):
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ArticleListCreateApiView(APIView):
+class ArticleListCreateAPIView(APIView):
 
   def get(self, request):
     articles = Article.objects.filter(active=True)
@@ -65,6 +66,35 @@ class ArticleListCreateApiView(APIView):
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailAPIView(APIView):
+
+  def get_object(self, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return article
+
+  
+  def post(self, request, pk):
+    article = self.get_object(pk)
+    serializer = ArticleSerializer(article)
+    return Response(serializer.data)
+  
+  def put(self, request, pk):
+    article = self.get_object(pk)
+
+    serializer = ArticleSerializer(article, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response( serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk):
+    article = self.get_object(pk)
+    article.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 
     
